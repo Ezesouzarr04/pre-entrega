a14aac6cf7ba986c64fd1a760d597ca85126441f
Ezequiel Souza Arraigada <ezequielsouza.es@gmail.com>
2024-05-29 12:49:31 -0300

    feat(Vehiculo): creación del modelo

--- Combi_escolar/Vehiculo/models.py
+++ Combi_escolar/Vehiculo/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Combi(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Pasajero(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Conductor(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Turno_de_recorrido(models.Model):
+    nombre = models.PositiveIntegerField(unique=True)
+    combi = models.ForeignKey(Combi, on_delete=models.SET_NULL, null=True, blank=True)
+    conductor = models.ForeignKey(Conductor, on_delete=models.SET_NULL, null=True, blank=True)
+    pasajero = models.ManyToManyField(Pasajero)
