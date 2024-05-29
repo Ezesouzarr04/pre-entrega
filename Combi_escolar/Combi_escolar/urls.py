916b4fff077aa0a65d7d591435de274b6af332b0
Ezequiel Souza Arraigada <ezequielsouza.es@gmail.com>
2024-05-29 16:27:57 -0300

    feat: CRUD funcionando

--- Combi_escolar/Combi_escolar/urls.py
+++ Combi_escolar/Combi_escolar/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
-    path("Vehiculo/", include("Vehiculo.urls")),
+    # path("", include("core.urls")),
+    # path("Vehiculo/", include("Vehiculo.urls")),
 ]
