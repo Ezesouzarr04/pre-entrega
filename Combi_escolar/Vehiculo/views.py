4539216aebc10df51c6a5150d95db8c1b532c2ca
Ezequiel Souza Arraigada <ezequielsouza.es@gmail.com>
2024-05-29 18:37:44 -0300

    feat: templates y urls

--- Combi_escolar/Vehiculo/views.py
+++ Combi_escolar/Vehiculo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+def index(request):
+    consulta = models.Turno_de_recorrido.objects.all()
+    contexto = {"TurnosDeRecorridos": consulta}
+    return render(request, "Vehiculo/index.html", contexto)
